a3d2874f52b985baddbdf063992ba81ead7a2425
ohsg0226 <ohsg0226@naver.com>
2023-06-26 13:06:54 +0900

    [feat] : 프로젝트 세팅

---
+++ GithubAction/GithubActionTests/GithubActionTests.swift
@@ -0,0 +1,24 @@
+//
+//  GithubActionTests.swift
+//  GithubActionTests
+//
+//  Created by 오승기 on 2023/06/26.
+//
+
+import XCTest
+@testable import GithubAction
+
+final class GithubActionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssert(true)
+    }
+}
